74906aab985e8d452fea184755381daf71a71ed0
Mohammad Amirdoust <mohamad.adk78@gmail.com>
2025-10-15 14:35:17 +0200

    - Setup Linter, Router, Localization, Riverpod

---
+++ lib/routing/router.dart
@@ -0,0 +1,26 @@
+import 'package:fitnest/routing/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:fitnest/ui/home/widgets/home_screen.dart';
+import 'package:fitnest/ui/auth/login/widgets/login_screen.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return _router;
+});
+
+final _router = GoRouter(
+  initialLocation: Routes.home,
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: Routes.home,
+      pageBuilder: (context, state) => MaterialPage(child: HomeScreen()),
+    ),
+    GoRoute(
+      path: Routes.login,
+      pageBuilder: (context, state) => MaterialPage(child: LoginScreen()),
+    ),
+  ],
+);
---
+++ lib/ui/core/localization/app_localization.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AppLocalization {
+  static AppLocalization of(BuildContext context) {
+    return Localizations.of(context, AppLocalization);
+  }
+
+  static const _strings = <String, String>{
+    'appName': 'Fitnest',
+  };
+
+  static String _get(String label) =>
+      _strings[label] ?? '[${label.toUpperCase()}]';
+
+  String get appName => _get('appName');
+
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalization> {
+  @override
+  bool isSupported(Locale locale) => locale.languageCode == 'en';
+
+  @override
+  Future<AppLocalization> load(Locale locale) {
+    return SynchronousFuture(AppLocalization());
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate<AppLocalization> old) =>
+      false;
+}
